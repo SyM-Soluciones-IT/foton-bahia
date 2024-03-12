7016b65b8f31b30059c860e4ec02bdc30175961a
Matias Peña <pena_matias@hotmail.com>
2024-03-12 19:22:07 -0300

    Cambio de react-app a react+vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000 // Especifica el puerto en el que deseas que se ejecute tu aplicación
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // Si `App.js` está dentro de `src`
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/App.jsx' // Especifica la ruta de tu archivo principal aquí
+      }
+    },
+  }
+});
