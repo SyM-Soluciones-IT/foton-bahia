e1075c05598c2879a4ab2fdbefd548cbf0f5ff50
Matias Peña <pena_matias@hotmail.com>
2024-04-05 16:34:04 -0300

    Pruebas para vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000 // Especifica el puerto en el que deseas que se ejecute tu aplicación
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') // Si `App.js` está dentro de `src`
